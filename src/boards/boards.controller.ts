96b8296d339c6fefee3fbaa81981a874b54839e1
a2175 <as217512@gmail.com>
2025-04-07 17:22:07 +0900

    ID를 이용해서 특정 게시물 가져오기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -3,6 +3,7 @@ import { BoardStatus } from './board-status.enum';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { Board } from './board.entity';
 
 @Controller('boards')
 export class BoardsController {
@@ -20,6 +21,11 @@ export class BoardsController {
     // ): Board {
     //     return this.boardsService.createBoard(createBoardDto);
     // }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: number): Promise<Board> {
+        return this.boardsService.getBoardById(id);
+    }
 
     // @Get('/:id')
     // getBoardById(@Param('id') id: string): Board {
